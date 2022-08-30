c6f6b4ba219298723c2806efce7dda52c47e67ad
Clover <phantasia1998@gmail.com>
2022-08-29 19:06:20 -0700

    BanList Update Aug 30th

--- banlist.js
+++ banlist.js
@@ -6,4 +6,72 @@ function takescreenshot() {
     
     }
 
-    $("li").click(function() { $(this).closest("li").remove(); }); 
+    $(".caption-style-1").on('click', 'li', function(){
+        $(".caption-style-2").append($(this).append());
+      });
+
+
+      $(".caption-style-2").on('click', 'li', function(){
+        $(".caption-style-1").append($(this).append());
+      });
+
+
+
+  function get_random_color() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
+
+
+$(".root2").css('borderColor', get_random_color());
+
+
+
+$(document).ready(function() {
+
+    var description = [
+        "render4.webp",
+        "render.webp",
+        "render6.webp",
+        "render8.webp"
+    ];
+  
+    var size = description.length
+    $('.item img').each(function() {
+      var x = Math.floor(size * Math.random())
+      if ($(this).hasClass("render")) {
+        $(this).attr("src", description[x]);
+      }
+
+
+    });
+
+  
+  });
+
+
+  $(document).ready(function() {
+
+    var description = [
+        "render3.webp",
+        "render2.webp",
+        "render5.webp",
+        "render7.webp"
+    ];
+  
+    var size = description.length
+    $('.item img').each(function() {
+      var x = Math.floor(size * Math.random())
+      if ($(this).hasClass("render2")) {
+        $(this).attr("src", description[x]);
+      }
+
+
+    });
+
+  
+  });
