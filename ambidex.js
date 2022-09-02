e528827bb84289e4d96c30e8be7831b060fd9413
Clover <phantasia1998@gmail.com>
2022-09-01 18:30:03 -0700

    Ambidex Update

--- ambidex.js
+++ ambidex.js
@@ -10,3 +10,44 @@ function myFunction() {
     } else { $(elem).hide(); } 
     });
     } 
+
+
+    var clicks = 0;
+function clickME() {
+    clicks -= 1;
+    document.getElementById("clicks").innerHTML = clicks;
+}
+
+
+var clicks = 0;
+function clickME5() {
+    clicks -= 2;
+    document.getElementById("clicks").innerHTML = clicks;
+}
+
+
+var clicks = 0;
+function clickME6() {
+    clicks -= 3;
+    document.getElementById("clicks").innerHTML = clicks;
+}
+
+
+
+var clicks = 0;
+function clickME2() {
+    clicks += 3;
+    document.getElementById("clicks").innerHTML = clicks;
+}
+
+var clicks = 0;
+function clickME3() {
+    clicks += 2;
+    document.getElementById("clicks").innerHTML = clicks;
+}
+
+var clicks = 0;
+function clickME4() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+}
